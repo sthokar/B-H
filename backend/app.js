3c06d5fba7910198f81aff67af29ccb199002531
sthokar <thokarsameer@yahoo.com>
2023-03-15 17:24:07 -0400

    post product to database

--- backend/app.js
+++ backend/app.js
@@ -6,5 +6,13 @@ app.use(express.json());
 const products = require('./routes/product');
 
 app.use('/api/v1', products)
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+  
+    res.status(500).json({
+      success: false,
+      error: 'Internal Server Error'
+    });
+  });
 
 module.exports = app;
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,23 @@
+const Product = require('../models/product')
+
+//Create new product => /api/v1/product/new
+
+exports.newProduct = async (req, res, next) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(201).json({
+            success:true,
+            product
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+
 exports.getProducts = (req, res, next) => {
     res.status(200).json({
         success:true,
         message: 'Get all products in database'
     })
 }
+
