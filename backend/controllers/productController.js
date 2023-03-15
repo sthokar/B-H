30357db04859b160d651b36d19fda95981799020
sthokar <thokarsameer@yahoo.com>
2023-03-15 17:49:03 -0400

    get single product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -14,10 +14,30 @@ exports.newProduct = async (req, res, next) => {
     }
 }
 
-exports.getProducts = (req, res, next) => {
+exports.getProducts = async (req, res, next) => {
+
+    const products = await Product.find();
     res.status(200).json({
         success:true,
-        message: 'Get all products in database'
+        products
     })
 }
+
+exports.getSingleProduct = async (req, res, next) => {
+
+    const product = await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(404).json({
+            success: false,
+            message: "Product not found"
+
+        })
+    }
+    res.status(200).json({
+        success:true,
+        product
+    })
+}
+
 
