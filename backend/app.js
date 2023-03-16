682f9b54b5215d7073e7e396993e2a34940499b1
sthokar <thokarsameer@yahoo.com>
2023-03-15 22:19:12 -0400

    error handler

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const app = express();
 
+const errorMiddleware = require('./middlewares/errors')
+
 app.use(express.json());
 
 const products = require('./routes/product');
 
 app.use('/api/v1', products)
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-  
-    res.status(500).json({
-      success: false,
-      error: 'Internal Server Error'
-    });
-  });
+
+//Middleware to handle errors
+app.use(errorMiddleware);
 
 module.exports = app;
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,8 @@
 const Product = require('../models/product')
 
+const ErrorHandler = require('../utils/errorHandler');
+const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+
 //Create new product => /api/v1/product/new
 
 exports.newProduct = async (req, res, next) => {
@@ -32,12 +35,8 @@ exports.getSingleProduct = async (req, res, next) => {
 
     const product = await Product.findById(req.params.id);
 
-    if(!product){
-        return res.status(404).json({
-            success: false,
-            message: "Product not found"
-
-        })
+    if (!product) {
+        return next(new ErrorHandler('Product not found', 404));
     }
     res.status(200).json({
         success:true,
@@ -91,5 +90,5 @@ exports.deleteProduct = async (req, res, next) => {
         message: 'Product is deleted.'
     })
 
-})
+}
 
